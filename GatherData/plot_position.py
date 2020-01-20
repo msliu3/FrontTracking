eb2f6211bce69760e98c8f9097af5cf062c5c1c8
msliu <liumingshanneo@163.com>
2020-01-20 12:52:12 +0800

    add gatherData.py odom and lidar modify the plot code

--- GatherData/plot_position.py
+++ GatherData/plot_position.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 import re
 import os
+import numpy as np
+import math
 
 
 def read_file_to_list(file):
@@ -40,13 +42,23 @@ if __name__ == '__main__':
     l_Y = []
     r_X = []
     r_Y = []
+    trans_matrix = np.ones((2, 2))
     for item in data:
         a_X.append(item[1])
         a_Y.append(item[2])
-        l_X.append(item[1]+item[5])
-        l_Y.append(item[2]+item[6])
-        r_X.append(item[1]+item[7])
-        r_Y.append(item[2]+item[8])
+        trans_matrix = [[math.cos(item[3]), math.sin(item[3])],
+                        [math.sin(-item[3]), math.cos(item[3])]]
+        trans_matrix_inv = np.linalg.inv(trans_matrix)
+        new_item = np.ones((2, 2))
+        new_item[0][0] = item[5]
+        new_item[1][0] = item[6]
+        new_item[0][1] = item[7]
+        new_item[1][1] = item[8]
+        new_xy_item = trans_matrix_inv * new_item
+        l_X.append(item[1]+new_xy_item[0][0])
+        l_Y.append(item[2]+new_xy_item[1][0])
+        r_X.append(item[1]+new_xy_item[0][1])
+        r_Y.append(item[2]+new_xy_item[1][1])
     plt.figure(1)
     plt.plot(a_Y, a_X, ls="-.")
     plt.plot(l_Y,l_X)
