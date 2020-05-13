f4e7990f17ae81a5c2101da13c127c41630f5a0a
msliu <liumingshanneo@163.com>
2020-05-13 20:52:34 +0800

    封装FrontFollowing模块 提供开启和暂停功能

--- testfile/FrontFollowingModule/FrontFollowing_1.py
+++ testfile/FrontFollowingModule/FrontFollowing_1.py
@@ -21,7 +21,7 @@ import os
 import sys
 
 pwd = os.path.abspath(os.path.abspath(__file__))
-father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + ".."+os.path.sep + "..")
+father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + ".." + os.path.sep + "..")
 print(father_path)
 sys.path.append(father_path)
 # 导入路径，确保ros打开能开启相应文件
@@ -61,8 +61,6 @@ class FrontFollowing(Process):
         # 事件控制：控制机器人是否执行移动逻辑
         self.event_action_loop = threading.Event()
 
-
-
         self.flag_stop = 1
         pass
 
@@ -76,40 +74,38 @@ class FrontFollowing(Process):
 
     def run(self) -> None:
         matcher = MC.MatchCase(foot=self.ir_camera.foot)
-        while True:
-            time.sleep(1)
-            print(matcher.leg.left_leg_x,matcher.leg.right_leg_x,id(matcher.leg))
-        # thread_ir = multiprocessing.Process(target=self.ir_camera.start_Demon_for_DL, args=(self.queue,))
-        # thread_ir.start()
-        # p1 = threading.Thread(target=self.action_loop, args=())
-        # p1.start()
-        # p2 = threading.Thread(target=self.detecting_loop, args=())  # nn_model,
-        # p2.start()
 
-    def action_loop(self):
+        thread_ir = multiprocessing.Process(target=self.ir_camera.start_Demon_for_DL, args=(self.queue,))
+        thread_ir.start()
+        p1 = threading.Thread(target=self.action_loop, args=(matcher,))
+        p1.start()
+        p2 = threading.Thread(target=self.detecting_loop, args=(matcher,))  # nn_model,
+        p2.start()
+
+    def action_loop(self,matcher):
         while True:
             print("------action_loop started------")
             self.event_action_loop.wait()
 
             # position_control负责计算目标位置，以及如何移动过去
             if self.position_control.action_over:
-                if self.matcher.back or self.matcher.forward:
+                if matcher.back or matcher.forward:
                     # 负责前进和后退的控制
 
                     # print("back_forward", position_control.expect_x, position_control.expect_theta)
                     self.position_control.action_forward_back(self.control_driver)
-                    self.matcher.clear_case()
+                    matcher.clear_case()
 
-                elif self.matcher.turning:
+                elif matcher.turning:
                     # 负责转弯的控制
 
                     # print("turn")
                     self.position_control.action_forward_and_turning(self.control_driver)
-                    self.matcher.clear_case()
+                    matcher.clear_case()
             self.event_action_loop.clear()
             print("------action_loop end------")
 
-    def detecting_loop(self):
+    def detecting_loop(self,matcher):
         nn_model = DL.DeepLearningDetectCase(model_name="dnn1.ckpt")
         while True:
             time.sleep(0.05)
@@ -118,16 +114,15 @@ class FrontFollowing(Process):
                 # print("into queue")
                 # matcher.clear_foot_and_leg()
                 temps = self.queue.get(block=False)
-                print("matcher id2", id(self.matcher))
                 print("leg:",
-                      self.matcher.leg.left_leg_x,
-                      self.matcher.leg.left_leg_y,
-                      self.matcher.leg.right_leg_x,
-                      self.matcher.leg.right_leg_y
+                      matcher.leg.left_leg_x,
+                      matcher.leg.left_leg_y,
+                      matcher.leg.right_leg_x,
+                      matcher.leg.right_leg_y
                       )
                 leg_temp = np.array(
-                    [self.matcher.leg.left_leg_x, self.matcher.leg.left_leg_y, self.matcher.leg.right_leg_x,
-                     self.matcher.leg.right_leg_y]).reshape([1, 4])
+                    [matcher.leg.left_leg_x, matcher.leg.left_leg_y, matcher.leg.right_leg_x,
+                     matcher.leg.right_leg_y]).reshape([1, 4])
 
                 ir_np = np.array(temps).reshape([32 * 24, nn_model.sample_num])
                 leg_np = np.array(leg_temp).reshape([4, nn_model.sample_num])
@@ -135,12 +130,14 @@ class FrontFollowing(Process):
                 # print("times: ",sum)
                 # result = 2
                 result = nn_model.print_predict_result(ir_np, leg_np)
-                self.matcher.detect_front_and_back_foot()
-                x, theta = self.matcher.detect_case(result)
+                matcher.detect_front_and_back_foot()
+                x, theta = matcher.detect_case(result)
                 print(x, theta)
                 self.position_control.set_expect(x, theta)
                 self.event_action_loop.set()
-                self.matcher.clear_expect()
+                matcher.clear_expect()
+
+
 if __name__ == '__main__':
     control_driver = CD.ControlDriver()
     control_driver.start()
